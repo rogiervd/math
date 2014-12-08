7d29dc2f64fb203e2c016137193c4f36407adac6
Rogier van Dalen <rcv25@cam.ac.uk>
2014-12-08 23:02:15 +0000

    Add max_semiring
    Ignore-this: 5817c6d82e6deee84d5a12c9b6637171
    
    darcs-hash:20141208230215-fa073-4db8a509c3a40be804a0b0ac907bd7523933a245

---
+++ include/math/max_semiring.hpp
@@ -0,0 +1,225 @@
+/*
+Copyright 2013, 2014 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Mathematical tools library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MATH_MAX_SEMIRING_HPP_INCLUDED
+#define MATH_MAX_SEMIRING_HPP_INCLUDED
+
+#include <limits>
+#include <iosfwd>
+#include <type_traits>
+
+#include <boost/mpl/bool.hpp>
+
+#include "magma.hpp"
+
+#include "detail/is_close.hpp"
+
+namespace math {
+
+/**
+Semiring whose \ref plus (and \ref choose) operation picks the maximum of the
+two values.
+\ref times performs multiplication on the underlying value.
+
+The underlying value must always be non-negative, so that the additive identity
+has value 0.
+The multiplicative identity has value 1.
+
+Division is only implemented for non-integer types.
+
+\tparam Type
+    Underlying type that represents the value.
+*/
+template <class Type> class max_semiring;
+
+template <class Type> struct max_semiring_tag;
+
+template <class Type> struct decayed_magma_tag <max_semiring <Type>>
+{ typedef max_semiring_tag <Type> type; };
+
+template <class Type> class max_semiring {
+private:
+    Type value_;
+public:
+    /**
+    Initialise with 0.
+    */
+    max_semiring() : value_()
+    { assert (value_ >= Type (0)); }
+
+    /**
+    Initialise with \a value as the value.
+    */
+    explicit max_semiring (Type const & value) : value_ (value) {}
+
+    /**
+    Return the underlying value.
+    */
+    Type const & value() const { return value_; }
+};
+
+namespace detail {
+
+    template <class Type> struct is_max_semiring_tag : boost::mpl::false_ {};
+    template <class Type> struct is_max_semiring_tag <max_semiring_tag <Type>>
+    : boost::mpl::true_ {};
+
+} // namespace detail
+
+MATH_MAGMA_GENERATE_OPERATORS (detail::is_max_semiring_tag)
+
+namespace operation {
+
+    /* Queries. */
+
+    template <class Type>
+        struct is_member <max_semiring_tag <Type>, typename std::enable_if <
+            std::numeric_limits <Type>::has_quiet_NaN>::type>
+    {
+        // Detect NaN.
+        bool operator() (max_semiring <Type> const & v) const { return v == v; }
+    };
+
+    template <class Type> struct equal <max_semiring_tag <Type>> {
+        bool operator() (
+            max_semiring <Type> const & left, max_semiring <Type> const & right)
+            const
+        { return left.value() == right.value(); }
+    };
+
+    template <class Type> struct approximately_equal <max_semiring_tag <Type>,
+        typename std::enable_if <!std::numeric_limits <Type>::is_exact>::type>
+    {
+        bool operator() (
+            max_semiring <Type> const & left, max_semiring <Type> const & right)
+            const
+        {
+            static Type const tolerance = 1e-5;
+            return math::detail::is_close (left.value(), right.value(),
+                tolerance);
+        }
+    };
+
+    // compare always orders smaller values first.
+    template <class Type> struct compare <max_semiring_tag <Type>> {
+        bool operator() (
+            max_semiring <Type> const & left, max_semiring <Type> const & right)
+            const
+        { return left.value() < right.value(); }
+    };
+
+    // "choose" and "plus" both select the maximal element.
+    template <class Type>
+    struct order <max_semiring_tag <Type>, callable::choose>
+    : reverse_order <compare <max_semiring_tag <Type>>> {};
+
+    template <class Type>
+    struct order <max_semiring_tag <Type>, callable::plus>
+    : reverse_order <compare <max_semiring_tag <Type>>> {};
+
+    /* Produce. */
+
+    // Return the not-a-number value, not-a-number, if it is available.
+    template <class Type> struct non_member <max_semiring_tag <Type>, typename
+        std::enable_if <std::numeric_limits <Type>::has_quiet_NaN>::type>
+    {
+        max_semiring <Type> operator() () const {
+            return max_semiring <Type> (
+                std::numeric_limits <Type>::quiet_NaN());
+        }
+    };
+
+    template <class Type>
+        struct identity <max_semiring_tag <Type>, callable::times>
+    {
+        max_semiring <Type> operator() () const
+        { return max_semiring <Type> (1); }
+    };
+
+    template <class Type>
+        struct identity <max_semiring_tag <Type>, callable::choose>
+    {
+        max_semiring <Type> operator() () const
+        { return max_semiring <Type> (0); }
+    };
+
+    // Additive identity: same as "choose".
+    template <class Type>
+        struct identity <max_semiring_tag <Type>, callable::plus>
+    : identity <max_semiring_tag <Type>, callable::choose> {};
+
+    // Multiplicative annihilator: forward to additive identity.
+    template <class Type>
+        struct annihilator <max_semiring_tag <Type>, callable::times>
+    : identity <max_semiring_tag <Type>, callable::plus> {};
+
+    /* Operations. */
+
+    // "choose" and "plus" are implemented automatically, since "order" is
+    // implemented for them.
+
+    template <class Type> struct times <max_semiring_tag <Type>>
+    : associative, commutative, approximate_if <
+        boost::mpl::bool_ <!std::numeric_limits <Type>::is_exact>>
+    {
+        max_semiring <Type> operator() (max_semiring <Type> const & left,
+            max_semiring <Type> const & right) const
+        { return max_semiring <Type> (left.value() * right.value()); }
+    };
+
+    // Semiring in both directions.
+    template <class Type> struct is_semiring <
+        max_semiring_tag <Type>, either, callable::times, callable::choose>
+    : rime::true_type {};
+
+    template <class Type> struct is_semiring <
+        max_semiring_tag <Type>, either, callable::times, callable::plus>
+    : rime::true_type {};
+
+    // Division is only implemented for non-integer types.
+    template <class Type> struct divide <max_semiring_tag <Type>, either,
+        typename std::enable_if <!std::numeric_limits <Type>::is_integer>::type>
+    : approximate_if <boost::mpl::bool_ <!std::numeric_limits <Type>::is_exact>>
+    {
+        max_semiring <Type> operator() (max_semiring <Type> const & left,
+            max_semiring <Type> const & right) const
+        { return max_semiring <Type> (left.value() / right.value()); }
+    };
+
+    // Inversion is only implemented for non-integer types.
+    template <class Type>
+        struct invert <max_semiring_tag <Type>, either, callable::times,
+        typename std::enable_if <!std::numeric_limits <Type>::is_integer>::type>
+    {
+        max_semiring <Type> operator() (max_semiring <Type> const & c) const
+        { return max_semiring <Type> (1 / c.value()); }
+    };
+
+    template <class Type> struct print <max_semiring_tag <Type>> {
+        template <class Stream>
+            void operator() (Stream & stream, max_semiring <Type> const & c)
+            const
+        { stream << c.value(); }
+    };
+
+} // namespace operation
+
+} // namespace math
+
+#endif // MATH_MAX_SEMIRING_HPP_INCLUDED
---
+++ test/math/test-max_semiring.cpp
@@ -0,0 +1,135 @@
+/*
+Copyright 2014 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Mathematical tools library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** \file
+Test max_semiring.hpp.
+*/
+
+#define BOOST_TEST_MODULE test_max_semiring
+#include "utility/test/boost_unit_test.hpp"
+
+#include "math/max_semiring.hpp"
+
+#include <boost/mpl/assert.hpp>
+
+#include "range/std/container.hpp"
+
+#include "math/check/check_magma.hpp"
+
+BOOST_AUTO_TEST_SUITE (test_suite_max_semiring)
+
+template <class Type> void check_max_semiring_for() {
+    typedef math::max_semiring <Type> semiring;
+
+    {
+        semiring five (5);
+        semiring three (3);
+        semiring zero (0);
+        semiring zero2;
+
+        BOOST_CHECK (zero == zero2);
+
+        BOOST_CHECK_EQUAL ((five * three).value(), 15);
+        BOOST_CHECK_EQUAL ((five + three), five);
+        BOOST_CHECK_EQUAL (math::choose (five, three), five);
+
+        BOOST_CHECK (three < five);
+        BOOST_CHECK (zero < five);
+        BOOST_CHECK (zero < three);
+    }
+
+    static_assert (math::has <math::callable::times (
+        semiring, semiring)>::value, "");
+    static_assert (math::has <math::callable::plus (
+        semiring, semiring)>::value, "");
+    static_assert (math::has <math::callable::choose (
+        semiring, semiring)>::value, "");
+
+    static_assert (math::is::associative <math::callable::times (
+        semiring, semiring)>::value, "");
+    static_assert (math::is::commutative <math::callable::times (
+        semiring, semiring)>::value, "");
+    static_assert (!math::is::idempotent <math::callable::times (
+        semiring, semiring)>::value, "");
+
+    static_assert (math::is::path_operation <math::callable::plus (
+        semiring, semiring)>::value, "");
+    static_assert (math::is::path_operation <math::callable::choose (
+        semiring, semiring)>::value, "");
+
+    std::vector <semiring> examples;
+    examples.push_back (semiring (0));
+    examples.push_back (semiring (3));
+    examples.push_back (semiring (5));
+    examples.push_back (semiring (17));
+
+    BOOST_CHECK (semiring (3) == semiring (3));
+
+    math::check_equal_on (examples);
+
+    math::check_semiring <semiring, math::either> (
+        math::times, math::plus, examples);
+    math::check_semiring <semiring, math::either> (
+        math::times, math::choose, examples);
+}
+
+BOOST_AUTO_TEST_CASE (test_max_semiring_complete) {
+    check_max_semiring_for <int>();
+    check_max_semiring_for <float>();
+    check_max_semiring_for <double>();
+}
+
+// Test whether floating-point numbers and integers are treated correctly when
+// they should behave differently.
+BOOST_AUTO_TEST_CASE (test_max_semiring_float) {
+    // non_member, divide, and invert available for floating-point numbers.
+    static_assert (math::has <
+        math::callable::non_member <math::max_semiring <double>>()>::value, "");
+    static_assert (math::has <
+        math::callable::invert <math::callable::times> (
+            math::max_semiring <double>)>::value, "");
+    static_assert (math::has <math::callable::divide<> (
+            math::max_semiring <double>, math::max_semiring <double>)>::value,
+        "");
+
+    // times and divide are approximate for floating-point numbers; plus is not.
+    static_assert (!math::is::approximate <math::callable::plus (
+        math::max_semiring <double>, math::max_semiring <double>)>::value, "");
+    static_assert (math::is::approximate <math::callable::times (
+        math::max_semiring <double>, math::max_semiring <double>)>::value, "");
+    static_assert (math::is::approximate <math::callable::divide<> (
+        math::max_semiring <double>, math::max_semiring <double>)>::value, "");
+
+    // But not for integers.
+    static_assert (!math::has <
+        math::callable::non_member <math::max_semiring <int>>()>::value, "");
+    static_assert (!math::has <math::callable::invert <math::callable::times> (
+            math::max_semiring <int>)>::value, "");
+    static_assert (!math::has <math::callable::divide<> (
+            math::max_semiring <int>, math::max_semiring <int>)>::value, "");
+
+    static_assert (!math::is::approximate <math::callable::plus (
+        math::max_semiring <int>, math::max_semiring <int>)>::value, "");
+    static_assert (!math::is::approximate <math::callable::times (
+        math::max_semiring <int>, math::max_semiring <int>)>::value, "");
+    static_assert (!math::is::approximate <math::callable::divide<> (
+        math::max_semiring <int>, math::max_semiring <int>)>::value, "");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
