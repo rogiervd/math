11d5a5daac2245c6b32d2604b2ae0440e98cd7e9
Rogier van Dalen <rcv25@cam.ac.uk>
2014-12-09 17:33:31 +0000

    Add "cost" semiring
    Ignore-this: 3b62b8243d7774f6ecd77d47ce79980d
    
    darcs-hash:20141209173331-fa073-022b0e9b0b5967ee8d901175d88c825c3c415044

---
+++ include/math/cost.hpp
@@ -0,0 +1,204 @@
+/*
+Copyright 2013, 2014 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Mathematical tools library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MATH_COST_HPP_INCLUDED
+#define MATH_COST_HPP_INCLUDED
+
+#include <algorithm>
+#include <limits>
+#include <iosfwd>
+#include <functional>
+
+#include <boost/utility/enable_if.hpp>
+
+#include "magma.hpp"
+
+#include "detail/is_close.hpp"
+
+namespace math {
+
+/**
+Semiring that is helpful to minimise a cost.
+It is also known as the "tropical" semiring.
+
+\ref times adds costs; \ref plus and \ref choose pick the lowest-cost argument.
+
+\tparam Type
+    Arithmetic type that represents the cost.
+    The type needs to be able to represent infinity, so that the additive
+    identity can be implemented.
+    <c>std::numeric_limits<Type>::has_infinity</c> must be \c true.
+*/
+template <class Type> class cost;
+
+template <class Type> struct cost_tag;
+
+template <class Type> struct decayed_magma_tag <cost <Type>>
+{ typedef cost_tag <Type> type; };
+
+template <class Type> class cost {
+private:
+    Type value_;
+
+    static_assert (std::numeric_limits <Type>::has_infinity,
+        "The additive identity is the infinity, which type 'Type' must have.");
+public:
+    /**
+    Initialise with infinite cost.
+    This results in the additive identity, also known as "zero".
+    \post <c>this->value() == std::numeric_limits<Type>::infinity()</c>
+    */
+    cost()
+    : value_(std::numeric_limits <Type>::infinity()) {}
+
+    /**
+    Initialise with a cost of \a value.
+    */
+    explicit cost (Type const & value) : value_ (value) {}
+
+    /**
+    Return the underlying value.
+    */
+    Type const & value() const { return value_; }
+};
+
+namespace detail {
+
+    template <class Type> struct is_cost_tag : boost::mpl::false_ {};
+    template <class Type> struct is_cost_tag <cost_tag <Type>>
+    : boost::mpl::true_ {};
+
+} // namespace detail
+
+MATH_MAGMA_GENERATE_OPERATORS (detail::is_cost_tag)
+
+namespace operation {
+
+    /* Queries. */
+
+    template <class Type>
+        struct is_member <cost_tag <Type>, typename boost::enable_if_c <
+            std::numeric_limits <Type>::has_quiet_NaN>::type>
+    {
+        // Detect NaN.
+        bool operator() (cost <Type> const & v) const { return v == v; }
+    };
+
+    template <class Type> struct equal <cost_tag <Type>> {
+        bool operator() (cost <Type> const & left, cost <Type> const & right)
+            const
+        { return left.value() == right.value(); }
+    };
+
+    template <class Type> struct approximately_equal <cost_tag <Type>> {
+        bool operator() (cost <Type> const & left, cost <Type> const & right)
+            const
+        {
+            static Type const tolerance = 1e-5;
+            return math::detail::is_close (left.value(), right.value(),
+                tolerance);
+        }
+    };
+
+    template <class Type> struct compare <cost_tag <Type>> {
+        bool operator() (cost <Type> const & left, cost <Type> const & right)
+            const
+        { return left.value() < right.value(); }
+    };
+
+    // "choose" and "plus" both select the lowest-cost element.
+    template <class Type> struct order <cost_tag <Type>, callable::choose>
+    : compare <cost_tag <Type>> {};
+
+    template <class Type> struct order <cost_tag <Type>, callable::plus>
+    : compare <cost_tag <Type>> {};
+
+    /* Produce. */
+
+    // Return the not-a-number value, not-a-number, if it is available.
+    template <class Type> struct non_member <cost_tag <Type>, typename
+        boost::enable_if_c <std::numeric_limits <Type>::has_quiet_NaN>::type>
+    {
+        cost <Type> operator() () const
+        { return cost <Type> (std::numeric_limits <Type>::quiet_NaN()); }
+    };
+
+    template <class Type> struct identity <cost_tag <Type>, callable::times>
+    { cost <Type> operator() () const { return cost <Type> (0); } };
+
+    template <class Type> struct identity <cost_tag <Type>, callable::choose> {
+        cost <Type> operator() () const
+        { return cost <Type> (std::numeric_limits <Type>::infinity()); }
+    };
+
+    template <class Type> struct identity <cost_tag <Type>, callable::plus>
+    : identity <cost_tag <Type>, callable::choose> {};
+
+    // Multiplicative annihilator: forward to additive identity.
+    template <class Type> struct annihilator <cost_tag <Type>, callable::times>
+    : identity <cost_tag <Type>, callable::plus> {};
+
+    /* Operations. */
+
+    // "choose" and "plus" are implemented implicitly, since "order" is
+    // implemented for them.
+
+    template <class Type> struct times <cost_tag <Type>>
+    : approximate, associative, commutative
+    {
+        cost <Type> operator() (
+            cost <Type> const & left, cost <Type> const & right) const
+        { return cost <Type> (left.value() + right.value()); }
+    };
+
+    // Semiring in both directions.
+    template <class Type> struct is_semiring <
+        cost_tag <Type>, either, callable::times, callable::choose>
+    : rime::true_type {};
+
+    template <class Type> struct is_semiring <
+        cost_tag <Type>, either, callable::times, callable::plus>
+    : rime::true_type {};
+
+    template <class Type> struct divide <cost_tag <Type>, either>
+    : approximate
+    {
+        cost <Type> operator() (
+            cost <Type> const & left, cost <Type> const & right) const
+        { return cost <Type> (left.value() - right.value()); }
+    };
+
+    template <class Type>
+        struct invert <cost_tag <Type>, either, callable::times>
+    {
+        cost <Type> operator() (cost <Type> const & c) const
+        { return cost <Type> (-c.value()); }
+    };
+
+    template <class Type> struct print <cost_tag <Type>> {
+        template <class Stream>
+            void operator() (Stream & stream, cost <Type> const & c) const
+        { stream << c.value(); }
+    };
+
+} // namespace operation
+
+} // namespace math
+
+#endif // MATH_COST_HPP_INCLUDED
---
+++ test/math/test-cost.cpp
@@ -0,0 +1,156 @@
+/*
+Copyright 2012, 2013 Rogier van Dalen.
+
+This file is part of Rogier van Dalen's Mathematical tools library for C++.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** \file
+Test cost.hpp.
+*/
+
+#define BOOST_TEST_MODULE test_cost
+#include "utility/test/boost_unit_test.hpp"
+
+#include <boost/test/floating_point_comparison.hpp>
+#include <boost/math/tools/test.hpp>
+
+#include "math/cost.hpp"
+
+#include <boost/mpl/assert.hpp>
+
+#include "range/std/container.hpp"
+
+#include "math/check/check_magma.hpp"
+
+BOOST_AUTO_TEST_SUITE (test_suite_cost)
+
+typedef math::cost <double> cost;
+
+// Test "choose", or that operation by any other name.
+template <class Choose> void test_choose (Choose const & choose) {
+    BOOST_MPL_ASSERT ((math::has <Choose (cost, cost)>));
+    BOOST_MPL_ASSERT_NOT ((math::is::approximate <Choose (cost, cost)>));
+    BOOST_MPL_ASSERT ((math::has <math::callable::identity <cost, Choose>()>));
+    BOOST_MPL_ASSERT ((math::has <math::callable::zero <cost>()>));
+    BOOST_MPL_ASSERT ((math::is::commutative <Choose, cost>));
+    BOOST_MPL_ASSERT ((math::is::associative <Choose, cost>));
+
+    BOOST_MPL_ASSERT ((math::is::path_operation <Choose, cost>));
+    BOOST_MPL_ASSERT ((math::is::idempotent <Choose, cost>));
+
+    BOOST_MPL_ASSERT_NOT ((math::has <math::callable::invert <Choose> (cost)>));
+    BOOST_MPL_ASSERT_NOT ((math::has <math::callable::invert<>(Choose, cost)>));
+
+    // Distributivity.
+    BOOST_MPL_ASSERT ((math::is::distributive <math::either,
+        math::callable::times, Choose, cost>));
+    BOOST_MPL_ASSERT ((math::is::distributive <math::left,
+        math::callable::times, Choose, cost>));
+    BOOST_MPL_ASSERT ((math::is::distributive <math::right,
+        math::callable::times, Choose, cost>));
+
+    BOOST_CHECK_EQUAL (choose (cost (0), cost (0)).value(), 0.);
+    BOOST_CHECK_EQUAL (choose (cost (-1), cost (0)).value(), -1.);
+    BOOST_CHECK_EQUAL (choose (cost (0), cost (-1)).value(), -1.);
+    BOOST_CHECK_EQUAL (choose (cost (5), cost (-1)).value(), -1.);
+    BOOST_CHECK_EQUAL (choose (cost (-1), cost (5)).value(), -1.);
+    BOOST_CHECK_EQUAL (choose (cost (2), cost (5)).value(), 2.);
+}
+
+BOOST_AUTO_TEST_CASE (test_cost) {
+    BOOST_MPL_ASSERT ((math::has <math::callable::non_member <cost>()>));
+    BOOST_MPL_ASSERT ((math::has <math::callable::is_member (cost)>));
+    BOOST_MPL_ASSERT ((math::has <math::callable::equal (cost, cost)>));
+    BOOST_MPL_ASSERT ((
+        math::has <math::callable::approximately_equal (cost, cost)>));
+
+    // "choose" and "plus" are the same.
+    test_choose (math::choose);
+    test_choose (math::plus);
+
+    // times.
+    BOOST_MPL_ASSERT ((math::has <math::callable::times (cost, cost &)>));
+    BOOST_MPL_ASSERT ((
+        math::is::approximate <math::callable::times (cost, cost &)>));
+    BOOST_MPL_ASSERT ((math::is::approximate <
+        math::callable::times (math::callable::times (cost, cost &), cost &)>));
+    BOOST_MPL_ASSERT ((
+        math::has <math::callable::identity <cost, math::callable::times>()>));
+    BOOST_MPL_ASSERT ((math::has <math::callable::one <cost>()>));
+
+    BOOST_MPL_ASSERT ((math::is::commutative <math::callable::times, cost>));
+    BOOST_MPL_ASSERT ((math::is::associative <math::callable::times, cost>));
+
+    BOOST_MPL_ASSERT_NOT ((
+        math::is::path_operation <math::callable::times, cost>));
+    BOOST_MPL_ASSERT_NOT ((math::is::idempotent <math::callable::times, cost>));
+
+    BOOST_MPL_ASSERT ((math::has <math::callable::divide<> (cost, cost)>));
+    BOOST_MPL_ASSERT ((
+        math::has <math::callable::invert <math::callable::times> (cost)>));
+    BOOST_MPL_ASSERT ((
+        math::has <math::callable::reverse <math::callable::times> (cost)>));
+
+    BOOST_CHECK_EQUAL (math::zero <cost>().value(),
+        std::numeric_limits <double>::infinity());
+    BOOST_CHECK_EQUAL (math::one <cost>().value(), 0.);
+    // Initialise as the additive identity, that is, with value "infinity".
+    BOOST_CHECK_EQUAL (cost().value(),
+        std::numeric_limits <double>::infinity());
+
+    cost a (3.);
+    cost b (5.);
+    cost c (-2);
+
+    // Multiplication: add costs.
+    BOOST_CHECK_EQUAL ((a*b).value(), 8.);
+    BOOST_CHECK_EQUAL (math::times (a, b).value(), 8.);
+    BOOST_CHECK_EQUAL (math::times (c, b), a);
+    // Division: subtract costs.
+    BOOST_CHECK_EQUAL ((a/a).value(), 0.);
+    BOOST_CHECK_EQUAL (math::divide (a, a).value(), 0.);
+    BOOST_CHECK_EQUAL (math::divide (b, c).value(), 7.);
+    // Invert element w.r.t. times: negate cost.
+    BOOST_CHECK_EQUAL (math::invert <math::callable::times> (a).value(), -3.);
+    BOOST_CHECK_EQUAL (math::invert (math::times, b).value(), -5.);
+    BOOST_CHECK_EQUAL (math::invert <math::left> (math::times, c).value(), 2.);
+    BOOST_CHECK_EQUAL (math::invert <math::right> (math::times, c).value(), 2.);
+
+    // Check for consistency.
+    std::vector <cost> examples;
+    examples.push_back (cost (-5.));
+    examples.push_back (cost (-2.3));
+    examples.push_back (cost (-1.));
+    examples.push_back (cost (-0.5));
+    examples.push_back (cost (-0.2));
+    examples.push_back (cost (0.));
+    examples.push_back (cost (0.2));
+    examples.push_back (cost (0.5));
+    examples.push_back (cost (1.));
+    examples.push_back (cost (2.3));
+    examples.push_back (cost (5.));
+
+    math::check_equal_on (examples);
+
+    math::check_magma <cost> (math::times, math::plus, examples);
+
+    math::check_semiring <cost, math::either> (
+        math::times, math::plus, examples);
+    math::check_semiring <cost, math::either> (
+        math::times, math::choose, examples);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
